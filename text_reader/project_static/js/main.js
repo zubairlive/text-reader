6f72691004a0c5a2e17336427855946eba355b0d
Zubair Afzal <zafzal@noon.com>
2022-10-30 23:58:55 +0500

    increase alret fadeout time

--- text_reader/project_static/js/main.js
+++ text_reader/project_static/js/main.js
@@ -2,5 +2,5 @@ $(document).ready(function () {
     // ============== Messages Timeout =============
     setTimeout(function () {
         $('.alert.alert-dismissable').fadeOut('slow');
-    }, 3000); // <-- time in milliseconds, 60000 =  1 minute
+    }, 5000); // <-- time in milliseconds, 60000 =  1 minute
 });
